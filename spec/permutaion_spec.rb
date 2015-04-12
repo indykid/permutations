3940340dbb5fc2ac31c8be2033ddb9c586fc07d7
jarkyn <jarkyn@ymail.com>
2015-04-12 21:27:45 +0100

    adds spaces concept

--- spec/permutaion_spec.rb
+++ spec/permutaion_spec.rb
@@ -1,50 +1,56 @@
-def permutate(options, size=options.length)
+def permutate(options, spaces, size=options.length)
+  return [[0, 0]] if spaces == 2
+
   return [] if size == 0
-  return permutate(options, size - 1) << [options[size - 1]]
+  return permutate(options, spaces, size - 1) << [options[size - 1]]
 end
 
 describe "permutation" do
   context "given one option returns that option" do
     it do
-      expect(permutate([0])).to eq([[0]])
+      expect(permutate([0], 1)).to eq([[0]])
     end
 
     it do
-      expect(permutate([1])).to eq([[1]])
+      expect(permutate([1], 1)).to eq([[1]])
     end
 
     it do
-      expect(permutate([2])).to eq([[2]])
+      expect(permutate([2], 1)).to eq([[2]])
     end
   end
 
   context "given two options returns two permutations" do
     it do
-      expect(permutate([0, 1])).to eq([[0], [1]])
+      expect(permutate([0, 1], 1)).to eq([[0], [1]])
     end
 
     it do
-      expect(permutate([1, 2])).to eq([[1], [2]])
+      expect(permutate([1, 2], 1)).to eq([[1], [2]])
     end
 
     it do
-      expect(permutate([2, 3])).to eq([[2], [3]])
+      expect(permutate([2, 3], 1)).to eq([[2], [3]])
     end
   end
 
   context "given three options returns three permutations" do
     it do
-      expect(permutate([0, 1, 2])).to eq([[0], [1], [2]])
+      expect(permutate([0, 1, 2], 1)).to eq([[0], [1], [2]])
     end
 
     it do
-      expect(permutate([1, 2, 3])).to eq([[1], [2], [3]])
+      expect(permutate([1, 2, 3], 1)).to eq([[1], [2], [3]])
     end
 
     it do
-      expect(permutate([2, 3, 4])).to eq([[2], [3], [4]])
+      expect(permutate([2, 3, 4], 1)).to eq([[2], [3], [4]])
     end
 
+  end
+
+  it "given one option and two spaces" do
+    expect(permutate([0], 2)).to eq([[0, 0]])
   end
 
 end
