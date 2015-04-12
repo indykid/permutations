8171e639eaf956ed9fa7fc600ecdae71b7ca9ed3
jarkyn <jarkyn@ymail.com>
2015-04-12 21:51:31 +0100

    removes duplication for 3 spaces case implementation

--- spec/permutaion_spec.rb
+++ spec/permutaion_spec.rb
@@ -1,8 +1,6 @@
 def permutate(options, spaces, size=options.length)
   return [[options[0], options[0]]] if spaces == 2
-  return [[0, 0, 0]] if spaces == 3 && options == [0]
-  return [[1, 1, 1]] if spaces == 3 && options == [1]
-  return [[2, 2, 2]] if spaces == 3 && options == [2]
+  return [[options[0], options[0], options[0]]] if spaces == 3
   return [] if size == 0
   return permutate(options, spaces, size - 1) << [options[size - 1]]
 end
