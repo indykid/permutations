9da7d26a08e11ee9709ba13b186d8b646f05eb6c
jarkyn <jarkyn@ymail.com>
2015-04-12 16:59:01 +0100

    adds solution for 1, 2 and 3 options

---
+++ spec/permutaion_spec.rb
@@ -0,0 +1,54 @@
+def permutate(options)
+  option = options[0]
+  return [[option]] if options == [option]
+  option2 = options[1]
+  return [[option], [option2]] if options == [option, option2]
+  option3 = options[2]
+  return [[option], [option2], [option3]] if options == [option, option2, option3]
+end
+
+describe "permutation" do
+  context "given one option returns that option" do
+    it do
+      expect(permutate([0])).to eq([[0]])
+    end
+
+    it do
+      expect(permutate([1])).to eq([[1]])
+    end
+
+    it do
+      expect(permutate([2])).to eq([[2]])
+    end
+  end
+
+  context "given two options returns two permutations" do
+    it do
+      expect(permutate([0, 1])).to eq([[0], [1]])
+    end
+
+    it do
+      expect(permutate([1, 2])).to eq([[1], [2]])
+    end
+
+    it do
+      expect(permutate([2, 3])).to eq([[2], [3]])
+    end
+  end
+
+  context "given three options returns three permutations" do
+    it do
+      expect(permutate([0, 1, 2])).to eq([[0], [1], [2]])
+    end
+
+    it do
+      expect(permutate([1, 2, 3])).to eq([[1], [2], [3]])
+    end
+
+    it do
+      expect(permutate([2, 3, 4])).to eq([[2], [3], [4]])
+    end
+
+  end
+
+end
